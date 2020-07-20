785ff95de18b2ca008098c796b481949e71297d6
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-20 10:03:42 +0200

    maven-compiler-plugin added to pom

--- src/main/java/com/michalgarnczarski/Main.java
+++ src/main/java/com/michalgarnczarski/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Generator planów");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
