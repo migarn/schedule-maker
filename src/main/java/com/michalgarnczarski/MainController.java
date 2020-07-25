01086e748ce6362887c475059900e7208c39463d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-25 17:34:02 +0200

    MainController commented. Format and default values for hour fields set

--- src/main/java/com/michalgarnczarski/MainController.java
+++ src/main/java/com/michalgarnczarski/MainController.java
@@ -4,11 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.converter.DateStringConverter;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class MainController {
 
@@ -22,32 +26,59 @@ public class MainController {
     Button addClassroomsListButton;
     @FXML
     Button editClassroomsListButton;
+    @FXML
+    TextField earliestHourField;
+    @FXML
+    TextField latestHourField;
 
     public void initialize() {
+
+        // Set hour fields format as HH:mm and set defaults values.
+
+        setHoursMinutesFormat(earliestHourField);
+        setHoursMinutesFormat(latestHourField);
+        earliestHourField.setText("8:00");
+        latestHourField.setText("20:00");
 
     }
 
     @FXML
     private void openAddClassesListWindow() {
+
+        // The method opens new window for adding new classes list
+
         openNewWindow("Dodaj listę zajęć", "/add-classes-list.fxml",300,275);
     }
 
     @FXML
     private void openEditClassesListWindow() {
+
+        // The method opens new window for editing a classes list
+
         openNewWindow("Edytuj listę zajęć", "/edit-classes-list.fxml",300,275);
     }
 
     @FXML
     private void openAddClassroomsListWindow() {
+
+        // The method opens new window for adding new classrooms list
+
         openNewWindow("Dodaj listę sal", "/add-classrooms-list.fxml",300,275);
     }
 
     @FXML
     private void openEditClassroomsListWindow() {
+
+        // The method opens new window for editing a classrooms list
+
         openNewWindow("Edytuj listę sal", "/edit-classrooms-list.fxml",300,275);
     }
 
     private void openNewWindow(String title, String path, int width, int height) {
+
+        // The method opens new window for given path with given title and window dimensions.
+        // New window is offset against its parent window.
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource(path));
@@ -64,5 +95,12 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setHoursMinutesFormat(TextField textField) {
+
+        // The method sets text field format as HH:mm
+
+        textField.setTextFormatter(new TextFormatter<>(new DateStringConverter(new SimpleDateFormat("HH:mm"))));
     }
 }
