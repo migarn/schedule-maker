9b76033db42eac5f1d230f48b52fbc5dc7a6003a
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-07-29 19:02:08 +0200

    ClassroomsList class added

---
+++ src/main/java/com/michalgarnczarski/ClassroomsList.java
@@ -0,0 +1,21 @@
+package com.michalgarnczarski;
+
+import java.util.Set;
+
+public class ClassroomsList {
+
+    private String name;
+    private Set<Classroom> classrooms;
+
+    public ClassroomsList(String name) {
+        this.name = name;
+    }
+
+    public Set<Classroom> getClassrooms() {
+        return classrooms;
+    }
+
+    public void setClassrooms(Set<Classroom> classrooms) {
+        this.classrooms = classrooms;
+    }
+}
